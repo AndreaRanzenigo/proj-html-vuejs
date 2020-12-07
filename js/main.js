0b71fbba22b34feb9330f1df9f6d8075bec36ad1
Andrea <andre98377@gmail.com>
2020-12-07 15:34:34 +0100

    section online vue

--- js/main.js
+++ js/main.js
@@ -25,6 +25,32 @@ const app = new Vue({
             'Purchase guide',
             'Privacy policy',
             'Teams of service'
+        ],
+        online: [
+            {
+                image: 'course-02.jpg',
+                price: '$40,',
+                cent: '00',
+                title: 'Learning to Write as a Professional Author',
+                lessons: '20 Lessons',
+                students: '50 Students'
+            },
+            {
+                image: 'stock-full-hd-03.jpg',
+                price: '$0,',
+                cent: '00',
+                title: 'Customer-centric Info-Tech Strategies',
+                lessons: '24 Lessons',
+                students: '769 Students'
+            },
+            {
+                image: 'stock-full-hd-04.jpg',
+                price: '$19,',
+                cent: '00',
+                title: 'Open Programming Courses for Everyone: Python',
+                lessons: '17 Lessons',
+                students: '62 Students'
+            }
         ]
     },
 }); 
